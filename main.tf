25693eef8b31de9990eb566ea6da47012a7a6a8b
rajangold02 <rajangold02@gmail.com>
2018-08-23 19:29:33 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,19 +1,14 @@
 terraform {
   backend "s3" {
     bucket = "wordpressterraform"
-    key    = "path/to/my/key"
+    key    = "key"
     region = "us-east-1"
   }
 }
 module "rds" {
-	source = "./modules/rds"
+        source = "./module/rds"
 }
 
 module "ec2" {
-	source = "./modules/ec2-wordpress-apache"
-}
-
-module "elb" {
-	source = "./modules/elb"
-	instance-id = "${module.ec2.instance-id}"
+        source = "./module/ec2"
 }
