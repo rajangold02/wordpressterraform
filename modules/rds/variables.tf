877a0c5e3338b2b2488630a3944e051aa06be963
rajangold02 <rajangold02@gmail.com>
2018-08-23 19:36:58 -0400

    Delete variables.tf

--- modules/rds/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "instance_class" {
-	description = "Type of instance"
-	default = "db.t2.micro"
-}
-variable "security_group_id" {
-	description = "Security Group you've created"
-	default = "sg-0350f96ea3a05c992"
-}
